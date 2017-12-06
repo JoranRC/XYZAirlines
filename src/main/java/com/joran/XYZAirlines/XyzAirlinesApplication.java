7f3b43cc758d772b202b514c89f4d895de2d7c36
JoranRC <33453681+JoranRC@users.noreply.github.com>
2017-12-06 09:17:51 +0100

    Initial commit

---
+++ src/main/java/com/joran/XYZAirlines/XyzAirlinesApplication.java
@@ -0,0 +1,12 @@
+package com.joran.XYZAirlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzAirlinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyzAirlinesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/joran/XYZAirlines/XyzAirlinesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.joran.XYZAirlines;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XyzAirlinesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
