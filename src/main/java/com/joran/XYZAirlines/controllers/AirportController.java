0f16dfe1fab36c7135abf7bcba963dd4584af793
JoranRC <33453681+JoranRC@users.noreply.github.com>
2017-12-06 10:09:08 +0100

    added AircraftController

--- src/main/java/com/joran/XYZAirlines/controllers/AirportController.java
+++ src/main/java/com/joran/XYZAirlines/controllers/AirportController.java
@@ -1,4 +1,35 @@
 package com.joran.XYZAirlines.controllers;
 
+import com.joran.XYZAirlines.models.Aircraft;
+import com.joran.XYZAirlines.models.Airport;
+import com.joran.XYZAirlines.repositories.AircraftRepository;
+import com.joran.XYZAirlines.repositories.AirportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/airport/")
 public class AirportController {
+
+    @Autowired
+    private AircraftRepository aircraftRepository;
+    @Autowired
+    private AirportRepository airportRepository;
+
+    @RequestMapping(value = "all", method = RequestMethod.GET)
+    public Iterable<Airport> index() {
+        return airportRepository.findAll();
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public void add(@RequestBody Airport airportToAdd) {
+        airportRepository.save(airportToAdd);
+    }
+
+    @RequestMapping(value = "delete/{airportName}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable String airportName) {
+        Airport airportToRemove = airportRepository.findOneByName(airportName);
+        airportRepository.delete(airportToRemove);
+    }
+
 }
--- src/main/java/com/joran/XYZAirlines/repositories/AirportRepository.java
+++ src/main/java/com/joran/XYZAirlines/repositories/AirportRepository.java
@@ -1,10 +1,11 @@
 package com.joran.XYZAirlines.repositories;
 
+import com.joran.XYZAirlines.models.Aircraft;
 import com.joran.XYZAirlines.models.Airport;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AirportRepository extends CrudRepository<Airport, Long> {
-
+    Airport findOneByName(String airportName);
 }
